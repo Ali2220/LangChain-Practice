cf418ee7bb82be6a20b0731c1adb0addb036729c
Ali2220 <alisarwar0277@gmail.com>
2025-11-14 17:38:13 +0500

    short-term-memory done

--- index.js
+++ index.js
@@ -35,24 +35,53 @@ dotenv.config();
 // console.log(response.content);
 
 // 3️⃣ Chains - Steps Ko Connect Karna.
+// import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
+// import { PromptTemplate } from "@langchain/core/prompts";
+
+// const model = new ChatGoogleGenerativeAI({
+//     model: "gemini-2.0-flash",
+//     apiKey: process.env.GEMINI_API_KEY
+// })
+
+// const template = PromptTemplate.fromTemplate(
+//     "Ek {adjective} {subject} ke baray mai khubsurat poem likho."
+// )
+
+// chain using pipe operator ()
+// const chain = template.pipe(model)
+
+// const result = await chain.invoke({
+//     adjective: "khubsurat",
+//     subject: "chandni raat"
+// })
+
+// console.log(result.content);
+
+
+// 4️⃣ Conversation Memory - Chat History - Short Term Memory
+
 import { ChatGoogleGenerativeAI } from "@langchain/google-genai";
-import { PromptTemplate } from "@langchain/core/prompts";
+import { HumanMessage, AIMessage } from "@langchain/core/messages";
+
 
 const model = new ChatGoogleGenerativeAI({
-    model: "gemini-2.0-flash",
+    model: 'gemini-2.0-flash',
     apiKey: process.env.GEMINI_API_KEY
 })
 
-const template = PromptTemplate.fromTemplate(
-    "Ek {adjective} {subject} ke baray mai khubsurat poem likho."
-)
+const chatHistory = []
 
-// chain using pipe operator ()
-const chain = template.pipe(model)
+// ➤ 1st message (User)
+chatHistory.push(new HumanMessage("Mera naam Ali hai."))
+const res1 = await model.invoke(chatHistory)
+chatHistory.push(new AIMessage(res1.content))
+console.log("Bot 1:",res1.content);
 
-const result = await chain.invoke({
-    adjective: "khubsurat",
-    subject: "chandni raat"
-})
 
-console.log(result.content);
+// ➤ 2nd message (User)
+chatHistory.push(new HumanMessage("Mera naam btao ?"))
+const res2 = await model.invoke(chatHistory)
+chatHistory.push(new AIMessage(res2.content))
+console.log("Bot 2:", res2.content);
+
+console.log(chatHistory);
